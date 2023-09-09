475aa9e4456d30de03adc2a927371ab2e4376c10
Wilshawn <wilshawn.alexander@gmail.com>
2023-09-08 22:02:54 -0400

    update proxy url

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
 
   // Search Query
   const searchQuery = async (query) => {
-    const res = await fetch(`https://cors-anywhere.herokuapp.com/https://api.genius.com/search?q=${query.text}`, {
+    const res = await fetch(`https://proxy-ibmasyzzya-uc.a.run.app/https://api.genius.com/search?q=${query.text}`, {
       method: 'GET',
       headers: {
-        'Authorization': 'Bearer -paZEmQPo5A8yPCcOcwGznNDlRQaFGT0xNzRalZlvtjJ_P9rwLnH8PYhAPA14os5',
+        'Authorization': 'Bearer -paZEmQPo5A8yPCcOcwGznNDlRQaFGT0xNzRalZlvtjJ_P9rwLnH8PYhAPA14os5'
       }
     });
     const data = await res.json();
